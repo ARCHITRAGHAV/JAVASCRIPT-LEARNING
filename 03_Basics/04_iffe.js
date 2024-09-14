e7d3e97ba30c700d12747c2e3df38a63818c5a5c
Archit Raghav <architraghav2004@gmail.com>
2024-09-14 12:55:20 +0530

    updated

---
+++ 03_Basics/04_iffe.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai() {
+    // named IIFE
+    console.log('DB CONNECTED');
+})();
+// global scope ke pollution ko hatane ke liye IIFE ka use hota hai
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})("archit")
