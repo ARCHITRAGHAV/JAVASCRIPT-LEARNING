9b489e46ebd3ea40a33402672f21287b2948c83d
Archit Raghav <architraghav2004@gmail.com>
2024-09-15 23:05:50 +0530

    updated

---
+++ 05_Iterations/07_some_practice_questions.js
@@ -0,0 +1,47 @@
+// let a = 32;
+// let b = 35692;
+
+// if ((a % 10 == 2) && (b % 10 == 2)) {
+//     console.log("Last Digit is 2");
+// }else{
+//     console.log("No");
+
+// }
+
+// let a = 2120, b = 120, c = 3450;
+// if ((a > b) && (a > c)) {
+//     console.log(a);
+// } else if (b > c) {
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
+
+// console.log(Math.floor(Math.random() * 6) + 1);
+
+// let arr = ["India", "Australia", "United Sates of America"]
+// let ansIndex = 0
+// for (let index = 0; index < arr.length; index++) {
+//     if (arr[index].length > arr[ansIndex].length) {
+//         ansIndex = index
+//     }
+// }
+// console.log(arr[ansIndex]);
+
+
+// let arr = [10, 20, 56, 58, 23, 24, 8, 9, 45]
+// let number = 30;
+// (() => {
+//     for (let index = 0; index < arr.length; index++) {
+//         if (arr[index] > number) {
+//             console.log(arr[index]);
+//         }
+//     }
+// })();
+
+// let arr = [1, 2, 3, 4, 5];
+// let result = 1;
+// for (let index = 0; index < arr.length; index++) {
+//     result = result * arr[index];
+// }
+// console.log(result);
