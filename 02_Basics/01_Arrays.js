177ba2adf6db88b20b681770d0c26c2707dfcd57
Archit Raghav <architraghav2004@gmail.com>
2025-02-24 23:12:45 +0530

    updated

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -18,7 +18,7 @@ const myArr2 = new Array(1, 2, 3, 4, 5)
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(19)); //returns index of asked value and always return -1 if asked value is not present in an array
 
-// const newArr = myArr.join()
+// const newArr = myArr.join() //Adds all the elements of an array into a string, separated by the specified separator string
 
 // console.log(myArr);
 // console.log(newArr);
