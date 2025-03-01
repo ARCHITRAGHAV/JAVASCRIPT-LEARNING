807bfae60531dc0ac90a4ab0499f3297ab404fe8
Archit Raghav <architraghav2004@gmail.com>
2025-03-01 15:26:14 +0530

    updated

--- 07_Projects/Color Changer/script.js
+++
@@ -1,4 +0,0 @@
-let buttons = document.querySelectorAll(".button")
-let body = document.querySelector("body");
-
-buttons.forEach((button) => button.addEventListener("click", (e) => body.style.backgroundColor = e.target.id))
---
+++ Projects/Project 2/script.js
@@ -0,0 +1,20 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let height = form.querySelector("#height").value;
+    let weight = form.querySelector("#weight").value;
+    let results = document.querySelector("#results");
+
+    let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    if (bmi < 18.6) {
+        results.innerHTML = `${bmi} and you are underweight.`;
+    }
+    else if (bmi >= 18.6 && bmi <= 24.9) {
+        results.innerHTML = `${bmi} and your weight is normal.`;
+    } else {
+        results.innerHTML = `${bmi} and you are overweight.`;
+    }
+})
