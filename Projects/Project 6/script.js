6c13b711a369e94a6b36bc7b0165ce9f31ab3220
Archit Raghav <architraghav2004@gmail.com>
2025-03-06 02:32:41 +0530

    updated

---
+++ Projects/Project 6/script.js
@@ -0,0 +1,20 @@
+function bgColorChanger() {
+    let body = document.querySelector("body");
+    let startBtn = document.querySelector("#start");
+    let stopBtn = document.querySelector("#stop");
+    let changeColor;
+    let colorChanger = () => {
+        let r = Math.floor(Math.random() * 255 + 1);
+        let g = Math.floor(Math.random() * 255 + 1);
+        let b = Math.floor(Math.random() * 255 + 1);
+        body.style.backgroundColor = `rgb(${r},${g},${b})`
+    }
+    startBtn.addEventListener("click", () => {
+        changeColor = setInterval(colorChanger, 1000);
+    })
+    stopBtn.addEventListener("click", () => {
+        clearInterval(changeColor);
+        changeColor = null;
+    })
+}
+bgColorChanger();
