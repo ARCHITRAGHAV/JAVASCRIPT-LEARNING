d317dacbeca7e0c5b07d7c51ec3ab119c4017f4c
Archit Raghav <architraghav2004@gmail.com>
2025-03-06 02:54:18 +0530

    project done

---
+++ Projects/Project 7/script.js
@@ -0,0 +1,18 @@
+let insert = document.querySelector(".insert");
+
+window.addEventListener("keypress", (e) => {
+    insert.innerHTML = `
+    <table>
+        <tr>
+            <th>Key</th>
+            <th>KeyCode</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${e.key === " " ? "space" : e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+    </table>
+`
+})
